c05b666895468da05efd19e551ca0a20d57c0fbf
Marcos Gómez <marnicgom@gmail.com>
2018-05-04 17:41:47 -0300

    Updated skynet-U3TP.py.

--- skynet-U3TP.py
+++ skynet-U3TP.py
@@ -16,16 +16,16 @@ def fuerzaBrutaPD(caracteres,longitud):
                 cache[n]=cache[n]+cache[n-1]
     return cache[longitud]
 def distLevenshtein(str1,str2):
-  d=dict()
-  for i in range(len(str1)+1):
-     d[i]=dict()
-     d[i][0]=i
-  for i in range(len(str2)+1):
-     d[0][i] = i
-  for i in range(1, len(str1)+1):
-     for j in range(1, len(str2)+1):
-        d[i][j] = min(d[i][j-1]+1, d[i-1][j]+1, d[i-1][j-1]+(not str1[i-1] == str2[j-1]))
-  print(d)
-  return d[len(str1)][len(str2)]
+    d=dict()
+    for i in range(len(str1)+1):
+        d[i]=dict()
+        d[i][0]=i
+        for i in range(len(str2)+1):
+            d[0][i] = i
+    for i in range(1, len(str1)+1):
+        for j in range(1, len(str2)+1):
+            d[i][j] = min(d[i][j-1]+1, d[i-1][j]+1, d[i-1][j-1]+(not str1[i-1] == str2[j-1]))
+    print(d)
+    return d[len(str1)][len(str2)]
 
 print(distLevenshtein("aserrejeja","aerrejeja"))
