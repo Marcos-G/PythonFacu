865baf4b49ec145a526281ef9cc562fa365afc6e
Marcos Gómez <marnicgom@gmail.com>
2018-05-11 16:05:17 -0300

    Updated U2TP1.py.

--- U2TP1.py
+++ U2TP1.py
@@ -1,7 +1,34 @@
-def ordenRaro(lista):
-    max=lista[0]
-    for i in range(1,length(lista)):
-        if(max<lista[i]):
-            max=lista[i]
-    print(max)
-ordenRaro([0,1,2,3,4])
+
+def esPar(x):
+    if x <= 0:
+        x = -x
+    return (x - (x % 1)) % 2 == 0
+
+def ordenRaro(vector):
+    ordenado = mergeSort(vector)
+    izq = LinkedList()
+    der = LinkedList()
+    while length(ordenado) > 1:
+        valor = pop(ordenado)
+        if esPar(valor):
+            add(der, valor)
+        else:
+            append(izq, valor)
+
+        medio = pop(ordenado)
+
+    while length(izq) > 0:
+        append(ordenado, pop(izq))
+
+    append(ordenado, medio)
+
+    while length(der) > 0:
+        append(ordenado, pop(der))
+list=LinkedList()
+append(list,3)
+append(list,36)
+append(list,32)
+append(list,45)
+append(list,0)
+print(list)
+print(orednRaro(list))
