71a216bf2499325a686d484f8275a45f03d9a6c3
Marcos Gómez <marnicgom@gmail.com>
2018-06-01 12:17:06 -0300

    Updated elecciones.py.

--- elecciones.py
+++ elecciones.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.QtCore import pyqtSlot
 
 class App(QWidget):
 
@@ -33,14 +33,14 @@ class App(QWidget):
         self.tableWidget = QTableWidget()
         self.tableWidget.setRowCount(4)
         self.tableWidget.setColumnCount(2)
-        self.tableWidget.setItem(0,0, QTableWidgetItem("Cell (1,1)").setFlags( Qt.ItemIsSelectable | Qt.ItemIsEditable | Qt.ItemIsEnabled ))
-        self.tableWidget.setItem(0,1, QTableWidgetItem("Cell (1,2)"))
-        self.tableWidget.setItem(1,0, QTableWidgetItem("Cell (2,1)"))
-        self.tableWidget.setItem(1,1, QTableWidgetItem("Cell (2,2)"))
-        self.tableWidget.setItem(2,0, QTableWidgetItem("Cell (3,1)"))
-        self.tableWidget.setItem(2,1, QTableWidgetItem("Cell (3,2)"))
-        self.tableWidget.setItem(3,0, QTableWidgetItem("Cell (4,1)"))
-        self.tableWidget.setItem(3,1, QTableWidgetItem("Cell (4,2)"))
+        self.tableWidget.setItem(0,0, QTableWidgetItem(0)
+        self.tableWidget.setItem(0,1, QTableWidgetItem(0))
+        self.tableWidget.setItem(1,0, QTableWidgetItem(0))
+        self.tableWidget.setItem(1,1, QTableWidgetItem(0))
+        self.tableWidget.setItem(2,0, QTableWidgetItem(0))
+        self.tableWidget.setItem(2,1, QTableWidgetItem(0))
+        self.tableWidget.setItem(3,0, QTableWidgetItem(0))
+        self.tableWidget.setItem(3,1, QTableWidgetItem(0))
         self.tableWidget.move(0,0)
 
         # table selection change
