d4b94d65b004bbe66e3a6832204ee2173922ee8a
Marcos Gómez <marnicgom@gmail.com>
2018-06-03 19:40:46 -0300

    Updated elecciones.py.

--- elecciones.py
+++ elecciones.py
@@ -1,59 +1,43 @@
-import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.lang import Builder
 
-class App(QWidget):
+Builder.load_string("""
 
-    def __init__(self):
-        super().__init__()
-        self.title = 'PyQt5 table - pythonspot.com'
-        self.left = 0
-        self.top = 0
-        self.width = 300
-        self.height = 200
-        self.initUI()
+<Test>:
+    size_hint: .5, .5
+    pos_hint: {'center_x': .5, 'center_y': .5}
+    do_default_tab: False
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+    TabbedPanelItem:
+        text: 'first tab'
+        Label:
+            text: 'First tab content area'
+    TabbedPanelItem:
+        text: 'tab2'
+        BoxLayout:
+            Label:
+                text: 'Second tab content area'
+            Button:
+                text: 'Button that does nothing'
+    TabbedPanelItem:
+        text: 'tab3'
+        RstDocument:
+            text:
+                '\\n'.join(("Hello world", "-----------",
+                "You are in the third tab."))
 
-        self.createTable()
+""")
 
-        # Add box layout, add table to box layout and add box layout to widget
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.tableWidget)
-        self.setLayout(self.layout)
 
-        # Show widget
-        self.show()
+class Test(TabbedPanel):
+    pass
 
-    def createTable(self):
-       # Create table
-        self.tableWidget = QTableWidget()
-        self.tableWidget.setRowCount(4)
-        self.tableWidget.setColumnCount(2)
-        '''
-        self.tableWidget.setItem(0,0, QTableWidgetItem("0")
-        self.tableWidget.setItem(0,1, QTableWidgetItem(0))
-        self.tableWidget.setItem(1,0, QTableWidgetItem(0))
-        self.tableWidget.setItem(1,1, QTableWidgetItem(0))
-        self.tableWidget.setItem(2,0, QTableWidgetItem(0))
-        self.tableWidget.setItem(2,1, QTableWidgetItem(0))
-        self.tableWidget.setItem(3,0, QTableWidgetItem(0))
-        self.tableWidget.setItem(3,1, QTableWidgetItem(0))
-        self.tableWidget.move(0,0)
-        '''
-        # table selection change
-        self.tableWidget.doubleClicked.connect(self.on_click)
 
-    @pyqtSlot()
-    def on_click(self):
-        print("\n")
-        for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+class TabbedPanelApp(App):
+    def build(self):
+        return Test()
+
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+    TabbedPanelApp().run()
