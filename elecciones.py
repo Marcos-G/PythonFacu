7f958d6d03d2ac2eb28c13330212df230992a4a5
Marcos Gómez <marnicgom@gmail.com>
2018-06-04 20:39:28 -0300

    Updated elecciones.py.

--- elecciones.py
+++ elecciones.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
 
 class SelectorFacultades(TabbedPanel):
-    tabla=[[[[[0] for q in range(10)] for w in range(5)] for e in range(5)] for r in range(13)]
+    tabla=[[[[0 for q in range(10)] for w in range(5)] for e in range(5)] for r in range(13)]
     print(tabla)
     pass
 
