32d2fb9d5ed790094c3a3d4fdf72a15d4f7dc8f7
Marcos Gómez <marnicgom@gmail.com>
2018-06-05 19:22:51 -0300

    Updated TP2U3.py.

--- TP2U3.py
+++ TP2U3.py
@@ -38,7 +38,7 @@ def radixSortPalabras(L):
         while(falta):
             falta=False
             for e in range(len(L)-1):
-                print(L[e])
+                print(L[e][p])
                 if(dicc[L[e][p]]>dicc[L[e+1][p]]):
                     temp=L[e+1]
                     L[e+1]=L[e]
