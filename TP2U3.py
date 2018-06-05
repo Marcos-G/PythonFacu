712f1a672c5eb5e215a0e5cddf3351c20f8afb93
Marcos Gómez <marnicgom@gmail.com>
2018-06-05 17:25:33 -0300

    Updated TP2U3.py.

--- TP2U3.py
+++ TP2U3.py
@@ -13,9 +13,9 @@ def radixSort(L):
             for e in range(len(L)-1):
                 if((L[e]%(10**p))//(10**(p-1))<(L[e+1]%(10**p))//(10**(p-1))):
                     print(e)
-                    L[e]+=L[e+1]
-                    L[e+1]-=L[e]
-                    L[e]-=L[e+1]
+                    temp=L[e+1]
+                    L[e+1]=L[e]
+                    L[e]=L[e+1]
                     falta=True
 
 def pasada(L,p,f):
