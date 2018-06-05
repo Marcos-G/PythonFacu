5a21eac75a6c33f2314fcf31c27b7e5196de2e57
Marcos Gómez <marnicgom@gmail.com>
2018-06-05 17:22:06 -0300

    Updated TP2U3.py.

--- TP2U3.py
+++ TP2U3.py
@@ -11,6 +11,7 @@ def radixSort(L):
         while(falta):
             falta=False
             for e in range(len(L)-1):
+                print("dsad")
                 if((L[e]%(10**p))//(10**(p-1))<(L[e+1]%(10**p))//(10**(p-1))):
                     L[e]+=L[e+1]
                     L[e+1]-=L[e]
