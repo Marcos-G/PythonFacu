33e87431932ae4d57aaebb7a7509326db9d71a0f
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 14:31:53 -0300

    Updated australiamalasya.pyit.py .

--- it.py
+++ it.py
@@ -1,10 +1,15 @@
 gram={'a':"al",'l':"paca",'p':"cp",'c':"pc"}
+gramaticas={0:gram}
 inp="a"
-for i in range(234612846789231):
+for i in range(5):
   arr=list(inp)
   outp=""
   for a in range(len(arr)):
     outp+=gram[arr[a]]
   inp=outp
+  print(outp)
   print(i/234612846789231.0)
 print(outp.count('alpaca'))
+"""110101010110000100001111011001001000001001101111
+123 ¡No encontrado! 43.4 hello ¡No encontrado! Hello Universe! World! New ¡No encontrado! ¡No encontrado! 34 9.845 ¡No encontrado! ¡No encontrado! 4$ 6$ ¡No encontrado! ¡No encontrado! ¡No encontrado!
+"""
