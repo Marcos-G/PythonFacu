08067bece2cd222c3c8f7f73ca5efe15af49b157
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 14:43:57 -0300

    Updated it.py.

--- it.py
+++ it.py
@@ -1,7 +1,8 @@
 gram={'a':"al",'l':"paca",'p':"cp",'c':"pc"}
 gramaticas={0:gram}
 inp="a"
-for i in range(10):
+c=0
+for i in range(234612846789231):
   arr=list(inp)
   outp=""
   once=True
@@ -11,9 +12,9 @@ for i in range(10):
         outp=""
         once=False
   inp=outp
+  c+=outp.count('alpaca')
   print(outp)
-  print(outp.count('alpaca'))
-print(outp.count('alpaca'))
+print(c)
 """110101010110000100001111011001001000001001101111
 234612846789231
 123 ¡No encontrado! 43.4 hello ¡No encontrado! Hello Universe! World! New ¡No encontrado! ¡No encontrado! 34 9.845 ¡No encontrado! ¡No encontrado! 4$ 6$ ¡No encontrado! ¡No encontrado! ¡No encontrado!
