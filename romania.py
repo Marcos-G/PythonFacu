2fc18dadf3cc189f193b78f652c2969de6a87b9c
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 20:56:11 -0300

    Updated romania.py.

---
+++ romania.py
@@ -0,0 +1,34 @@
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+        gcd = b
+    return gcd, x, y
+
+def main():
+
+    p = 62674694810582163977646464092292439405182466161502960359408571447710174165909
+    q = 57248512388615138300979959427360676128469
+    e = 10001
+    ct = 018060085203065057140124128117050100007074060101142162017115068097148382172249154167157090250
+
+    # compute n
+    n = p * q
+
+    # Compute phi(n)
+    phi = (p - 1) * (q - 1)
+
+    # Compute modular inverse of e
+    gcd, a, b = egcd(e, phi)
+    d = a
+
+    print( "n:  " + str(d) );
+
+    # Decrypt ciphertext
+    pt = pow(ct, d, n)
+    print( "pt: " + str(pt) )
+
+if __name__ == "__main__":
+    main()
