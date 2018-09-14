287fdebd11e89bb7670e7e1bbf42c652ea935629
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 15:33:18 -0300

    Updated it.py.

--- it.py
+++ it.py
@@ -3,13 +3,22 @@ gramaticas={0:gram}
 inp="a"
 c=0
 s=True
-for i in range(0,234612846789231,2):
+'''for i in range(0,234612846789231,2):
     c*=4
     c+=1
     if(c>123456789):
         c=c%123456789
     print(i/234612846789231.0)
-print(c)
+print(c)'''
+s=1
+for i in range(1,1909991):
+    s*=2
+    if(s):
+        s+=1
+    else:
+        s-=1
+    print(i," ",s)
+
 """110101010110000100001111011001001000001001101111
 234612846789231
 123 ¡No encontrado! 43.4 hello ¡No encontrado! Hello Universe! World! New ¡No encontrado! ¡No encontrado! 34 9.845 ¡No encontrado! ¡No encontrado! 4$ 6$ ¡No encontrado! ¡No encontrado! ¡No encontrado!
