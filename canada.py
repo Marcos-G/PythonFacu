41d7dd18ecf38d89833fa61a381dae458f37b1f3
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 03:31:54 -0300

    Updated canada.pymy1.png .

--- canada.py
+++ canada.py
@@ -27,34 +27,7 @@ outp=[[inp[i*10+5,j*10+5] for j in range(100)] for i in range(100)]
 outp=np.asarray(outp)
 img = Image.fromarray(outp, 'RGB')
 img.save('my.png')
-moverPieza(outp,18,1,0,0)
-moverPieza(outp,0,16,0,4)
-moverPieza(outp,1,4,0,10)
-moverPieza(outp,1,9,0,9)
-moverPieza(outp,2,8,0,7)
-moverPieza(outp,3,0,0,14)
-moverPieza(outp,4,7,0,3)
-moverPieza(outp,5,3,0,17)
-moverPieza(outp,7,17,0,13)
-moverPieza(outp,8,8,0,12)
-moverPieza(outp,10,1,0,5)
-moverPieza(outp,11,11,0,8)
-moverPieza(outp,12,16,0,16)
-moverPieza(outp,13,6,0,11)
-moverPieza(outp,15,5,0,1)
-moverPieza(outp,16,5,0,2)
-moverPieza(outp,17,4,0,15)
-moverPieza(outp,17,12,0,18)
-moverPieza(outp,18,10,0,6)
-moverPieza(outp,19,17,0,19)
-moverPieza(outp,4,14,19,19)
-moverPieza(outp,8,5,18,19)
-moverPieza(outp,5,10,17,19)
-moverPieza(outp,9,16,16,19)
-moverPieza(outp,4,7,15,19)
-moverPieza(outp,3,16,14,19)
-moverPieza(outp,19,5,3,3)
-#moverPieza(outp,9,0,19,17)
+
 
 for a in range(1):
     for i in range(20):
@@ -65,7 +38,9 @@ for a in range(1):
                     break
                 for k in range(20):
                     if(o>i or k>j):
-                        if(j==0):
+                        if(j==0 and i==0):
+                            moverPieza(outp,18,1,0,0)
+                        elif(j==0):
                             if(bon(getColor(outp,4,o,k)) and bon(getColor(outp,1,o,k)) and np.array_equal(getColor(outp,2,o,k),getColor(outp,3,i-1,j)) ):
                                 moverPieza(outp,o,k,i,j)
                                 cortar=True
