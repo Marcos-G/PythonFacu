a928895b3d4db10a8b7b3751bb16ae937e27cf4a
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 00:46:18 -0300

    Updated canada.pymy1.png .

--- canada.py
+++ canada.py
@@ -28,20 +28,20 @@ outp=np.asarray(outp)
 img = Image.fromarray(outp, 'RGB')
 img.save('my.png')
 #moverPieza(outp,7,17,0,0)
-for i in range(20):
-    for j in range(20):
-        for o in range(20):
-            for k in range(20):
-                if(i==0):
-                    if(np.array_equal(getColor(outp,1,o,k),getColor(outp,2,i,j-1)) and np.array_equal(getColor(outp,4,o,k),getColor(outp,3,i,j-1))):
-                        moverPieza(outp,o,k,i,j)
-                elif(j==0):
-                    if(np.array_equal(getColor(outp,2,o,k),getColor(outp,3,i-1,j)) and np.array_equal(getColor(outp,1,o,k),getColor(outp,4,i-1,j))):
-                        moverPieza(outp,o,k,i,j)
-
-                else:
-                    if((np.array_equal(getColor(outp,2,o,k),getColor(outp,3,i-1,j)) and np.array_equal(getColor(outp,1,o,k),getColor(outp,4,i-1,j))) or (np.array_equal(getColor(outp,1,o,k),getColor(outp,2,i,j-1)) and np.array_equal(getColor(outp,4,o,k),getColor(outp,3,i,j-1)))):
-                        moverPieza(outp,o,k,i,j)
+for a in range(10):
+    for i in range(20):
+        for j in range(20):
+            for o in range(20):
+                for k in range(20):
+                    if(i==0):
+                        if(np.array_equal(getColor(outp,1,o,k),getColor(outp,2,i,j-1)) and np.array_equal(getColor(outp,4,o,k),getColor(outp,3,i,j-1))):
+                            moverPieza(outp,o,k,i,j)
+                    elif(j==0):
+                        if(np.array_equal(getColor(outp,2,o,k),getColor(outp,3,i-1,j)) and np.array_equal(getColor(outp,1,o,k),getColor(outp,4,i-1,j))):
+                            moverPieza(outp,o,k,i,j)
+                    else:
+                        if((np.array_equal(getColor(outp,2,o,k),getColor(outp,3,i-1,j)) and np.array_equal(getColor(outp,1,o,k),getColor(outp,4,i-1,j))) or (np.array_equal(getColor(outp,1,o,k),getColor(outp,2,i,j-1)) and np.array_equal(getColor(outp,4,o,k),getColor(outp,3,i,j-1)))):
+                            moverPieza(outp,o,k,i,j)
 
 
 
