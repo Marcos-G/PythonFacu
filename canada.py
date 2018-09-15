185eaed37dcc51cf1be422baf6487e5675e24bba
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 23:29:23 -0300

    Updated canada.pymy1.png .

--- canada.py
+++ canada.py
@@ -26,8 +26,8 @@ outp=[[inp[i*10+5,j*10+5] for j in range(100)] for i in range(100)]
 outp=np.asarray(outp)
 img = Image.fromarray(outp, 'RGB')
 img.save('my.png')
-for i in range(20):
-    for j in range(1):
+for i in range(1):
+    for j in range(20):
         romper=False
         for o in range(i,20):
             if romper:
@@ -38,7 +38,7 @@ for i in range(20):
                         moverPieza(outp,o,k,i,j)
                         romper=True
                         break
-                elif(j==0):
+                elif(i==0):
                     if(bon(getColor(outp,1,o,k)) and bon(getColor(outp,2,o,k)) and np.array_equal(getColor(outp,4,o,k),getColor(outp,3,i-1,j))):
                         moverPieza(outp,o,k,i,j)
                         romper=True
