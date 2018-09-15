1e249ebda1332d47f572e7ce9fa6c95bbce0941c
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 02:36:37 -0300

    Updated canada.py.

--- canada.py
+++ canada.py
@@ -47,7 +47,7 @@ moverPieza(outp,17,4,0,5)
 moverPieza(outp,17,12,0,3)
 moverPieza(outp,18,10,0,13)
 moverPieza(outp,19,17,0,19)
-moverPieza(outp,11,0,19,19)
+moverPieza(outp,4,14,19,19)
 
 
 for a in range(3):
