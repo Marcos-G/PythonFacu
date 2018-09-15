c48a5572313b0a5aa3cebc8819070fd6c8258465
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 19:15:10 -0300

    Updated guyana.py.

--- guyana.py
+++ guyana.py
@@ -4,7 +4,7 @@ binary_file =open("data", "rb")
     # Read the whole file at once
 data = binary_file.read(4)
 print(data)
-print(struct.unpack('ui', data))
+print(struct.unpack('I', data))
 '''data=bytearray(data)
 for i in range(len(data)):
     data[i]=data[i]^137
