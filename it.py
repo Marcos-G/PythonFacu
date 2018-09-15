c3ca20664da644744384c86ced7ce263ae5ba089
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 20:29:41 -0300

    Updated it.py.

--- it.py
+++ it.py
@@ -12,28 +12,4 @@ for i in range(50):
 supern="110101010110000100001111011001001000001001101110"[::-1]
 acu=0
 for i in range(len(supern)):
-    print("an+"+str(i)+"=an*2^"+str(i)+"+"+str(J[i])
-'''
-for i in range(400):
-    print((2**i)%123456789)
-for i in range(0,234612846789231,2):
-    c*=4
-    c+=1
-    if(c>123456789):
-        c=c%123456789
-    print(i/234612846789231.0)
-print(c)
-s=0
-for i in range(1,1909991):
-    s*=2
-    if(b):
-        s+=1
-    else:
-        s-=1
-    b=not b
-    print(i/1909991.0)
-print(s)
-110101010110000100001111011001001000001001101111
-234612846789231
-123 ¡No encontrado! 43.4 hello ¡No encontrado! Hello Universe! World! New ¡No encontrado! ¡No encontrado! 34 9.845 ¡No encontrado! ¡No encontrado! 4$ 6$ ¡No encontrado! ¡No encontrado! ¡No encontrado!
-'''
+    print("an+"+str(i)+"=an*2^"+str(i)+"+"+str(J[i]))
