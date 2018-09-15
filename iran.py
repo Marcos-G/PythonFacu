98df535530d2a8945db735ae6277f9369e1b0bba
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 13:10:50 -0300

    Updated iran.py.

--- iran.py
+++ iran.py
@@ -10,15 +10,15 @@ resultador=""
 print(len(inp))
 for i in range(23):
     for j in range(24):
-        if(inp[40*i:40*i+40,40*j:40*j+40].tostr() in pokenumbers):
-            resultado+=str(pokenumbers[inp[40*i:40*i+40,40*j:40*j+40].tostr()])
-            resultador+=str(pokenumbers[inp[40*i:40*i+40,40*j:40*j+40].tostr()])[::-1]
+        if(str(inp[40*i:40*i+40,40*j:40*j+40]) in pokenumbers):
+            resultado+=str(pokenumbers[str(inp[40*i:40*i+40,40*j:40*j+40])])
+            resultador+=str(pokenumbers[str(inp[40*i:40*i+40,40*j:40*j+40])])[::-1]
         else:
             img = Image.fromarray(inp[40*i:40*i+40,40*j:40*j+40], 'RGB')
             img.show()
             n=input()
             print(i*24+j,"/",23*24)
-            pokenumbers[inp[40*i:40*i+40,40*j:40*j+40].tostr()]=n
+            pokenumbers[str(inp[40*i:40*i+40,40*j:40*j+40])]=n
 print(resutado)
 print()
 print(resultador)
