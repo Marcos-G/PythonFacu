6915a4b2b4e0589835557dc7beea0a70db7cb839
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 18:50:33 -0300

    Updated alemania.pyromania.py .

--- romania.py
+++ romania.py
@@ -1,33 +1,44 @@
-def egcd(a, b):
-    x,y, u,v = 0,1, 1,0
-    while a != 0:
-        q, r = b//a, b%a
-        m, n = x-u*q, y-v*q
-        b,a, x,y, u,v = a,r, u,v, m,n
-        gcd = b
-    return gcd, x, y
+def extended_gcd(a, b):
+    """Returns pair (x, y) such that xa + yb = gcd(a, b)"""
+    x, lastx, y, lasty = 0, 1, 1, 0
+    while b != 0:
+        q, r = divmod(a, b)
+        a, b = b, r
+        x, lastx = lastx - q * x, x
+        y, lasty = lasty - q * y, y
+    return lastx, lasty
 
-def main():
 
-    p = 62674694810582163977646464092292439405182466161502960359408571447710174165909
+def multiplicative_inverse(e, n):
+    """Find the multiplicative inverse of e mod n."""
+    x, y = extended_gcd(e, n)
+    if x < 0:
+        return n + x
+    return x
+
+
+def rsa_generate_key():
+    p = 1094782941871623486260250734009229761
     q = 57248512388615138300979959427360676128469
-    e = 10001
-    ct = 299604539773691895576847697095098784338054746292313044353582078965
-
-    # compute n
+    # Ensure q != p, though for large values of bits this is
+    # statistically very unlikely
+    
     n = p * q
-
-    # Compute phi(n)
     phi = (p - 1) * (q - 1)
+    # Here we pick a random e, but a fixed value for e can also be used.
+    while True:
+        e = 10001
+        if fractions.gcd(e, phi) == 1:
+            break
+    d = multiplicative_inverse(e, phi)
+    return (n, e, d)
 
-    # Compute modular inverse of e
-    gcd, a, b = egcd(e, phi)
-    d = a
 
-    print( "n:  " + str(d) );
+def rsa_encrypt(message, n, e):
+    return modular.power(message, e, n)
 
-    # Decrypt ciphertext
-    print( "pt: " + str(pt) )
 
-if __name__ == "__main__":
-    main()
+def rsa_decrypt(cipher, n, d):
+    return modular.power(cipher, d, n)
+msg="<UËA9 |u2dJ<e¢sDaž¬ù§Zú"
+(n,e,d)=rsa_generate_keys
