9729de44bf6eefed872b5818a77a4f79e2545330
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 11:57:18 -0300

    Updated iran.py.

--- iran.py
+++ iran.py
@@ -10,15 +10,15 @@ resultador=""
 print(len(inp))
 for i in range(23):
     for j in range(24):
-        if(inp[40*i:40*i+40,40*j:40*j+40].tobytes() in pokenumbers):
-            resultado+=str(pokenumbers[inp[40*i:40*i+40,40*j:40*j+40].tobytes()])
-            resultador+=str(pokenumbers[inp[40*i:40*i+40,40*j:40*j+40].tobytes()])[::-1]
+        if(inp[40*i+1:40*i+41,40*j+1:40*j+41].tobytes() in pokenumbers):
+            resultado+=str(pokenumbers[inp[40*i+1:40*i+41,40*j+1:40*j+41].tobytes()])
+            resultador+=str(pokenumbers[inp[40*i+1:40*i+41,40*j+1:40*j+41].tobytes()])[::-1]
         else:
-            img = Image.fromarray(inp[40*i:40*i+40,40*j:40*j+40], 'RGB')
+            img = Image.fromarray(inp[40*i+1:40*i+41,40*j+1:40*j+41], 'RGB')
             img.show()
             n=input()
-            print()
-            pokenumbers[inp[40*i:40*i+40,40*j:40*j+40].tobytes()]=n
+            print(i*24+j,"/",23*24)
+            pokenumbers[inp[40*i+1:40*i+41,40*j+1:40*j+41].tobytes()]=n
 print(resutado)
 print()
 print(resultador)
