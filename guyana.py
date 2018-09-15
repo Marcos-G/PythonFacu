a0e119965e6fe1d1405bb7e992cfad1b7672b8ac
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 15:54:39 -0300

    Updated guyana.py.

--- guyana.py
+++ guyana.py
@@ -1,4 +1,4 @@
 with open("secreto.pdf", "rb") as binary_file:
     # Read the whole file at once
     data = binary_file.read()
-    print(data[0],data[1],data[2],data[3])
+    print(data[0],data[0]^1,data[0]^2,data[0]^3)
