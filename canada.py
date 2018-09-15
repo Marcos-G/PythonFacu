d64d32aae4c8c3ecd332be3b2f9abdc82ae34c02
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 23:22:00 -0300

    Updated canada.pymy1.png .

--- canada.py
+++ canada.py
@@ -5,9 +5,7 @@ import copy
 
 def moverPieza(im,i1,j1,i2,j2):
     temp=copy.deepcopy(im[i1*5:i1*5+5,j1*5:j1*5+5])
-    print(temp[0,0])
     im[i1*5:i1*5+5,j1*5:j1*5+5]=im[i2*5:i2*5+5,j2*5:j2*5+5]
-    print(temp[0,0])
     im[i2*5:i2*5+5,j2*5:j2*5+5]=temp
 def getColor(im,n,i,j):
     a=0
@@ -24,14 +22,8 @@ def bon(col):
 
 
 inp = misc.imread('rompecabezas.png')
-print(inp[1,1])
 outp=[[inp[i*10+5,j*10+5] for j in range(100)] for i in range(100)]
 outp=np.asarray(outp)
-print(getColor(outp,1,0,0))
-print(getColor(outp,2,0,0))
-print(getColor(outp,3,0,0))
-print(getColor(outp,4,0,0))
-
 img = Image.fromarray(outp, 'RGB')
 img.save('my.png')
 for i in range(1):
