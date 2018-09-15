7b19c991715ad531a2af81086d9195b32b86d123
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 16:01:04 -0300

    Updated guyana.py.

--- guyana.py
+++ guyana.py
@@ -1,7 +1,6 @@
-with open("secreto.pdf", "rb") as binary_file:
+with open("secreto.pdf", "rwb") as binary_file:
     # Read the whole file at once
     data = binary_file.read()
     for i in range(len(data)):
         data[i]=data[i]^137
     num_bytes_written = binary_file.write(data)
-    print("Wrote %d bytes." % num_bytes_written)
