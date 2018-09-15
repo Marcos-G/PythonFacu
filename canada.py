e1a33eb3eb6c7453ae98b3e043d856d6aa51a02c
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 22:07:34 -0300

    Updated canada.py.

--- canada.py
+++ canada.py
@@ -1,9 +1,10 @@
 from scipy import misc
 from PIL import Image
 import numpy as np
+import copy
 
 def moverPieza(im,i1,j1,i2,j2):
-    temp=im[i1*5:i1*5+5,j1*5:j1*5+5]
+    temp=copy.deepcopy(im[i1*5:i1*5+5,j1*5:j1*5+5])
     print(temp[0,0])
     im[i1*5:i1*5+5,j1*5:j1*5+5]=im[i2*5:i2*5+5,j2*5:j2*5+5]
     print(temp[0,0])
