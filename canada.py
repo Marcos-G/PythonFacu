75c7f64902dbcecc819576c0d9c28873079575a1
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 22:03:34 -0300

    Updated canada.py.

--- canada.py
+++ canada.py
@@ -2,6 +2,12 @@ from scipy import misc
 from PIL import Image
 import numpy as np
 
+def moverPieza(im,i1,j1,i2,j2):
+    temp=im[i1*5:i1*5+5,j1*5:j1*5+5]
+    im[i1*5:i1*5+5,j1*5:j1*5+5]=im[i2*5:i2*5+5,j2*5:j2*5+5]
+    im[i2*5:i2*5+5,j2*5:j2*5+5]=temp
+
+
 inp = misc.imread('rompecabezas.png')
 print(inp[1,1])
 outp=[[inp[i*10+5,j*10+5] for j in range(100)] for i in range(100)]
@@ -13,7 +19,3 @@ img.save('my.png')
 moverPieza(outp,0,0,0,1)
 img = Image.fromarray(temp, 'RGB')
 img.save('my1.png')
-def moverPieza(im,i1,j1,i2,j2):
-    temp=im[i1*5:i1*5+5,j1*5:j1*5+5]
-    im[i1*5:i1*5+5,j1*5:j1*5+5]=im[i2*5:i2*5+5,j2*5:j2*5+5]
-    im[i2*5:i2*5+5,j2*5:j2*5+5]=temp
