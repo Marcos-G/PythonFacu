3b1048ecd76fec27bcd9a55514b8896a441c87a7
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 22:03:15 -0300

    Updated canada.py.

--- canada.py
+++ canada.py
@@ -9,10 +9,11 @@ outp=np.asarray(outp)
 
 img = Image.fromarray(outp, 'RGB')
 img.save('my.png')
-img.show()
-temp=outp[0:5,0:5]
+#img.show()
+moverPieza(outp,0,0,0,1)
 img = Image.fromarray(temp, 'RGB')
 img.save('my1.png')
-def moverPieza(i1,j1,i2,j2):
-    temp=outp[i1*5:i1*5+5,j1*5:j1*5+5]
-    outp[i1*5:i1*5+5,j1*5:j1*5+5]=outp[i2*5:i2*5+5,j2*5:j2*5+5]
+def moverPieza(im,i1,j1,i2,j2):
+    temp=im[i1*5:i1*5+5,j1*5:j1*5+5]
+    im[i1*5:i1*5+5,j1*5:j1*5+5]=im[i2*5:i2*5+5,j2*5:j2*5+5]
+    im[i2*5:i2*5+5,j2*5:j2*5+5]=temp
