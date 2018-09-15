e275b394f3365a1c4b7fa4720d0c0f520508daf2
Marcos Gómez <marnicgom@gmail.com>
2018-09-14 21:37:40 -0300

    Updated canada.py.

--- canada.py
+++ canada.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 inp = misc.imread('rompecabezas.png')
 print(inp[1,1])
-outp=for i in range(100)[for j in range(100)[inp[i*50+5,j*50+5]]]
+outp=[for i in range(100):[for j in range(100):inp[i*50+5,j*50+5]]]
 img = Image.fromarray(outp, 'RGB')
 img.save('my.png')
 img.show()
