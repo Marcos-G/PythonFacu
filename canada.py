11b99162a120087bc2526e027a98ca4555760aa2
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 01:01:41 -0300

    Updated canada.pymy1.png .

--- canada.py
+++ canada.py
@@ -29,10 +29,10 @@ img = Image.fromarray(outp, 'RGB')
 img.save('my.png')
 moverPieza(outp,1,18,0,0)
 for a in range(1):
-    for i in range(20):
+    for i in range(1):
         for j in range(20):
-            for o in range(20):
-                for k in range(20):
+            for o in range(i,20):
+                for k in range(j,20):
                     if(i==0):
                         if(bon(getColor(outp,1,o,k)) and bon(getColor(outp,4,o,k)) and np.array_equal(getColor(outp,2,o,k),getColor(outp,3,i-1,j))):
                             moverPieza(outp,o,k,i,j)
