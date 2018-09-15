e2f2e518e55688297e5651b9b93af02ae7cd6a70
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 11:39:56 -0300

    Updated iran.py.

--- iran.py
+++ iran.py
@@ -6,11 +6,13 @@ import numpy as np
 inp = misc.imread('test.png')
 pokenumbers={}
 resultado=""
+resultador=""
 print(len(inp))
 for i in range(23):
     for j in range(24):
         if(inp[40*i:40*i+40,40*j:40*j+40].tobytes() in pokenumbers):
             resultado+=pokenumbers[inp[40*i:40*i+40,40*j:40*j+40].tobytes()]
+            resultador+=string(pokenumbers[inp[40*i:40*i+40,40*j:40*j+40].tobytes()]).reverse()
         else:
             img = Image.fromarray(inp[40*i:40*i+40,40*j:40*j+40], 'RGB')
             img.show()
