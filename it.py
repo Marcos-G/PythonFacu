fb1c2bb038de785ce000e662d4d5906eeb9bb323
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 16:51:13 -0300

    Updated it.py.

--- it.py
+++ it.py
@@ -4,8 +4,9 @@ inp="a"
 c=0
 b=True
 for i in range(10):
-    c=c*2+(-1)**i
     print(i+1,c)
+    c=c*2+(-1)**i
+
 '''for i in range(400):
     print((2**i)%123456789)
 for i in range(0,234612846789231,2):
