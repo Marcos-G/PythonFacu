648e9cc446c3023b98f7637520a1d41d964d1090
Marcos Gómez <marnicgom@gmail.com>
2018-09-16 01:07:08 -0300

    Updated JAPAN.py.

--- JAPAN.py
+++ JAPAN.py
@@ -1,13 +1,17 @@
-n=0
-d=1
-acu=0
-i=0
-while(i<27):
-    n*=42
-    d*=42
-    print(n,"/",d)
-    n+=i
-    print(n,"/",d)
-    acu+=((1.0/42.0)**i)*(i/42.0)
-    i+=1
-    print(acu)
+import copy
+def contarFallas(n,arr):
+    if(n==27):
+        if( count(arr,1)<27):
+            return 1
+        else:
+            return 0
+    else:
+        con=0
+        for i in range 42:
+            array=deepcopy(arr)
+            j=i
+            while(j<42 and array[j]==1):
+                j+=1
+            if(j<42):
+                array[j]=1
+            cont+=contarFallas(n+1,array)
