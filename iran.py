b36553d785caf41503eaf9059dfaab083ab7e9b7
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 22:41:03 -0300

    Updated iran.py.

--- iran.py
+++ iran.py
@@ -1,43 +1,12 @@
-import cv2
+from scipy import misc
 import numpy as np
-from matplotlib import pyplot as plt
 
-img = cv2.imread('test.jpg',0)
-img2 = img.copy()
-template = img[0:40,0:40]
-w, h = template.shape[::-1]
-
-# All the 6 methods for comparison in a list
-methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
-
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
-
-    # Apply template Matching
-    res = cv2.matchTemplate(img,template,method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-
-    cv2.rectangle(img,top_left, bottom_right, 255, 2)
-
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-
-    plt.show()
-'''
 inp = misc.imread('test.jpg')
 pokenumbers={}
+for i in range(9200):
+    for j in range(9200):
+        if(np.array_equal(inp[i:i+400,j:j+400],inp[0:400,0:400])):
+            print("uno")
 resultado=""
 resultador=""
 print(len(inp))
@@ -55,4 +24,3 @@ for i in range(23):
 print(resutado)
 print()
 print(resultador)
-'''
