4eb76d1960878a160d89c34da6d4d1c718c32b59
Marcos Gómez <marnicgom@gmail.com>
2018-09-16 02:42:50 -0300

    Updated JAPAN.py.

--- JAPAN.py
+++ JAPAN.py
@@ -1,27 +1,25 @@
 import copy
 prog=0
 
-def contarFallas(n,arr):
+def contarFallas(n,arr,s):
     global prog
     prog+=1
     print(prog)
-    if(n==27):
-        if( arr.count(1)<27):
-            return 1/(42^27)
+    if(n==s):
+        if( arr.count(1)<s):
+            return 1
         else:
             return 0
     else:
         con=0
-        for i in range(42):
+        for i in range(s):
             array=copy.deepcopy(arr)
             j=i
-            while(j<42 and array[j]==1):
+            while(j<s  and array[j]==1):
                 j+=1
-            if(j<42):
+            if(j<s):
                 array[j]=1
-                con+=contarFallas(n+1,array)
-            else:
-                con+=1/(42^(n+1))
+            con+=contarFallas(n+1,array)
 
         return con
 print(contarFallas(0,[0 for i in range(42)]))
