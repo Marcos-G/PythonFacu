c45c7957d73e2dae3639a3c273d777740e5cfc3a
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 22:16:09 -0300

    Updated it.py.

--- it.py
+++ it.py
@@ -1,7 +1,10 @@
+gram={'a':"al",'l':"paca",'p':"cp",'c':"pc"}
+gramaticas={0:gram}
+inp="a"
 c=0
 b=True
 J={1:1}
-supern="1111"[::-1]
+supern="11010101011000010000111101100100100000100111110"[::-1]
 acu=0
 check=0
 for i in range(len(supern)):
@@ -11,11 +14,10 @@ for i in range(len(supern)):
     c=c%123456789
     J[2**(i+1)]=c
     if(supern[i]=='1'):
-        check+=2**i
+        check+=2**(i)
+
         acu*=h
         acu+=J[2**i]
         acu=acu%123456789
-        print(i,c,acu)
-print(check)
-print(acu)
-print(str(J))
+
+print(check,acu)
