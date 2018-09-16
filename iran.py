9d02382d77e55f8af1c163051a1b87637bc7075b
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 23:12:20 -0300

    Updated iran.py.

--- iran.py
+++ iran.py
@@ -18,7 +18,7 @@ for i in range(23):
             calculados.append((i,j))
             for a in range(23):
                 for b in range(24):
-                    if( (i,j) not in calculados and (inp[40*a:40*a+40,40*b:40*b+40]-inp[40*i:40*i+40,40*j:40*j+40]).sum()<3000)
+                    if( (i,j) not in calculados and (inp[40*a:40*a+40,40*b:40*b+40]-inp[40*i:40*i+40,40*j:40*j+40]).sum()<=3000)
                         res[i][j]=n
                         calculados.append((i,j))
             print(str(sol))
