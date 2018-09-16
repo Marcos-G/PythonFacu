8bdf1e4ff7bcc7540a85316c01a58d6de9435276
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 21:06:26 -0300

    Updated it.py.

--- it.py
+++ it.py
@@ -18,6 +18,7 @@ for i in range(len(supern)):
     if(supern[i]=='1'):
         check+=2**(i)
         print("an+"+str(2**(i))+"=an*"+str(2**(2*i))+"+"+str(J[i*2]))
+        print(check)
         acu=(acu*(2**(2*i))+J[i*2])
         print(acu)
 print(acu)
