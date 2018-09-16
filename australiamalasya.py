60ce1ff659713776af708d3b77879cd35fb5142c
Marcos Gómez <marnicgom@gmail.com>
2018-09-16 02:43:30 -0300

    Updated australiamalasya.py.

--- australiamalasya.py
+++ australiamalasya.py
@@ -31,6 +31,7 @@ def get_tag_value(f):
                     dict_tag_value['%s~%s' %(curr_tag_name, tag_name)] = tag_val
             if taglist:
                 prev_tag_name_list.append(curr_tag_name)
+    print(dict_tag_value)
     for count in range(int(num_query)):
         entry = f.readline().strip()
         if entry in dict_tag_value:
@@ -39,7 +40,7 @@ def get_tag_value(f):
             print ('¡No encontrado!', end=chr(0x0A))
 
 
-for i in range(1,16):
+for i in range(9,10):
     print("archivo",i)
     n="input-"
     if(i<10):
