f5829367b8cf90c6ea45c30e31a738cb6a6f1772
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 21:53:33 -0300

    Updated it.py.

--- it.py
+++ it.py
@@ -6,6 +6,7 @@ b=True
 J={1:1}
 supern="110101010110000100001111011001001000001001101111"[::-1]
 acu=0
+check=0
 for i in range(len(supern)):
     print("pang")
     h=pow(2,2**i,123456789)
@@ -23,17 +24,3 @@ for i in range(len(supern)):
         c=c%123456789
         print(acu)
     print(str(J))
-
-#supern="11010"[::-1]
-acu=0
-check=0
-for i in range(len(supern)):
-    if(supern[i]=='1'):
-        check+=2**(i)
-        print("an+"+str(2**(i))+"=an*"+str(2**(2**(i)))+"+"+str(J[2**i]))
-        print(check)
-        acu=(acu*(2**(2**i))+J[2**i])%123456789
-        print(acu)
-print(acu)
-print(check)
-'''
