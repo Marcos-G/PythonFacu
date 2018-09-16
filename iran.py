0f7a709158d570c5a82faa1a2ba5cc27b82610a0
Marcos Gómez <marnicgom@gmail.com>
2018-09-16 18:22:41 -0300

    Updated iran.py.

--- iran.py
+++ iran.py
@@ -28,7 +28,7 @@ res=""
 resi=""
 for i in range(len(array)):
     for j in range(len(array[i])):
-        res+=str(chr(int(array[i][j])))
-        resi+=str(chr(int(array[i][j][::-1])))
+        res+=str(int(array[i][j],16))
+        resi+=str(int(array[i][j][::-1],16))
 print(res)
 print(resi)
