4b9b48a6d7729c9f1679fb181fafdf0c25946bdf
Marcos Gómez <marnicgom@gmail.com>
2018-09-15 22:19:55 -0300

    Updated it.py.

--- it.py
+++ it.py
@@ -4,7 +4,7 @@ inp="a"
 c=0
 b=True
 J={1:1}
-supern="11010101011000010000111101100100100000100111110"[::-1]
+supern="110101010110000100001111011001001000001001101110"[::-1]
 acu=0
 check=0
 for i in range(len(supern)):
@@ -14,7 +14,7 @@ for i in range(len(supern)):
     c=c%123456789
     J[2**(i+1)]=c
     if(supern[i]=='1'):
-        check+=2**(i)
+        check+=2**i
         acu*=h
         acu+=J[2**i]
         acu=acu%123456789
