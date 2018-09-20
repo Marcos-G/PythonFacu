298b834a1bd921c2157552f48abb6bd32b4d126b
Marcos Gómez <marnicgom@gmail.com>
2018-09-20 16:22:50 -0300

    Updated algeria.py.

---
+++ algeria.py
@@ -0,0 +1,15 @@
+import random
+m=0
+h=0
+n=1
+while(m<1000 or h<1000):
+    if(random.randint(0,1)==1):
+        m+=1
+    gana=True
+    for i in range(n):
+        if(random.randint(0,1)==0):
+            gana=False
+            break
+    if(gana):
+        h+=2**(n-1)
+    print(m,h)
