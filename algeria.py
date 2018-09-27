444a1c994a0ed0c722bacc05394878bdf0f16cfd
Marcos Gómez <marnicgom@gmail.com>
2018-09-26 21:11:56 -0300

    Updated algeria.py.

--- algeria.py
+++ algeria.py
@@ -5,9 +5,10 @@ def probMujerGanarAntesDe(n):
         pm=0
     else:
         pm=binom(n,1000)*(pow(0.5,n))
+    return pm
 def probHombreGanarTurno(n,t):
     tg=ceil(1000/(2**(t-1)))
-    ph=binom(n-1,tg-1)*pow(pow(0.5,t),tg)*pow((1-pow(0.5,t),n-tg))
+    ph=binom(n-1,tg-1)*pow(pow(0.5,t),tg)*pow(1-pow(0.5,t),n-tg)
     return ph
 h=11
 m=1
