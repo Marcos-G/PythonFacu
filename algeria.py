83c1f10bc242b53e06c47a30f50f783c9b380fc1
Marcos Gómez <marnicgom@gmail.com>
2018-09-27 19:26:23 -0300

    Updated algeria.py.

--- algeria.py
+++ algeria.py
@@ -8,6 +8,12 @@ def probMujerNOGanarEnOAntesDe(n):
     else:
         pm=comb(n,n-1000,exact=True)*(Decimal(0.5)**n)
     return pm
+def probMujerGanarEnOAntesDe(n):
+    if(n<1000):
+        pm=1
+    else:
+        pm=comb(n,1000,exact=True)*(Decimal(0.5)**n)
+    return pm
 def probHombreGanarTurno(n,t):
     tg=ceil(1000/(2**(t-1)))
     ph=comb(n-1,tg-1,exact=True)*((Decimal(0.5)**t)**tg)*(1-Decimal(0.5)**t)**(n-tg)
@@ -17,5 +23,5 @@ m=1
 p=0
 setcontext(Context(prec=60, rounding=ROUND_HALF_DOWN))
 for n in range(1,3000):
-    p=probMujerNOGanarEnOAntesDe(n)
+    p=probMujerNOGanarEnOAntesDe(n)+probMujerGanarEnOAntesDe(n)
     print(n,p)
