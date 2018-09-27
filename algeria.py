0195508ba51b60077b618d93049f47f93a973a7c
Marcos Gómez <marnicgom@gmail.com>
2018-09-26 23:02:27 -0300

    Updated algeria.py.

--- algeria.py
+++ algeria.py
@@ -11,12 +11,12 @@ def probMujerGanarAntesDe(n):
     return pm
 def probHombreGanarTurno(n,t):
     tg=ceil(1000/(2**(t-1)))
-    ph=binom(n-1,tg-1)*pow(pow(0.5,t),tg)*pow(1-pow(0.5,t),n-tg)
+    ph=comb(n-1,tg-1,exact=True)*((Decimal(0.5)**t)**tg)*(1-Decimal(0.5)**t)**(n-tg)
     return ph
 h=11
 m=1
 p=0
 for n in range(1,2000):
-    p*=probHombreGanarTurno(n)*(1-probMujerGanarAntesDe(n))
+    p*=probHombreGanarTurno(n,h)*(1-probMujerGanarAntesDe(n))
     print(p)
 print(sys.float_info)
