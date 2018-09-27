e028134a527ffd2bc02b26bfe229933b62f0f602
Marcos Gómez <marnicgom@gmail.com>
2018-09-27 19:20:43 -0300

    Updated algeria.py.

--- algeria.py
+++ algeria.py
@@ -2,11 +2,11 @@ from scipy.special import binom,comb
 from math import ceil
 from decimal import *
 import sys
-def probMujerGanarEnOAntesDe(n):
+def probMujerNOGanarEnOAntesDe(n):
     if(n<1000):
-        pm=0
+        pm=1
     else:
-        pm=Decimal(comb(n,1000,exact=True))*(Decimal(0.5)**n)
+        pm=((comb(n,n-1000,exact=True)*(Decimal(0.5)**n))
     return pm
 def probHombreGanarTurno(n,t):
     tg=ceil(1000/(2**(t-1)))
@@ -17,7 +17,7 @@ m=1
 p=0
 setcontext(Context(prec=60, rounding=ROUND_HALF_DOWN))
 for n in range(1,3000):
-    p+=(Decimal(0.5)**11)*((1-Decimal(0.5)**11)**(n-1))*((comb(n,n-1000,exact=True)*(Decimal(0.5)**n)))
+    p+=(Decimal(0.5)**11)*((1-Decimal(0.5)**11)**(n-1))*probMujerNOGanarEnOAntesDe(n)
     a=Decimal(comb(n,1000,exact=True)*(Decimal(0.5)**n))
     b=Decimal(Decimal(1.0)-Decimal(a))
     c=Decimal(Decimal(1.0)-Decimal(b))
