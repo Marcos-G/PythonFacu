2e98bab65a0544ddd41b5a3312bbcc6031686e63
Marcos Gómez <marnicgom@gmail.com>
2018-09-27 19:24:46 -0300

    Updated algeria.py.

--- algeria.py
+++ algeria.py
@@ -17,8 +17,5 @@ m=1
 p=0
 setcontext(Context(prec=60, rounding=ROUND_HALF_DOWN))
 for n in range(1,3000):
-    p+=(Decimal(0.5)**11)*((1-Decimal(0.5)**11)**(n-1))*probMujerNOGanarEnOAntesDe(n)
-    a=Decimal(comb(n,1000,exact=True)*(Decimal(0.5)**n))
-    b=Decimal(Decimal(1.0)-Decimal(a))
-    c=Decimal(Decimal(1.0)-Decimal(b))
+    p=probMujerNOGanarEnOAntesDe(n)
     print(n,p)
