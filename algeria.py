e198eaf7db2725e0082c3f190f3f0be245b6a4a5
Marcos Gómez <marnicgom@gmail.com>
2018-09-27 19:11:17 -0300

    Updated algeria.py.

--- algeria.py
+++ algeria.py
@@ -16,8 +16,8 @@ h=11
 m=1
 p=0
 setcontext(Context(prec=60, rounding=ROUND_HALF_DOWN))
-for n in range(1,3000):
-    p+=(Decimal(0.5)**11)*((1-Decimal(0.5)**11)**(n-1))*((comb(n,n-1000,exact=True)*(Decimal(0.5)**n)))
+for n in range(1,300000):
+    p+=((comb(n,n-1000,exact=True)*(Decimal(0.5)**n)))
     a=Decimal(comb(n,1000,exact=True)*(Decimal(0.5)**n))
     b=Decimal(Decimal(1.0)-Decimal(a))
     c=Decimal(Decimal(1.0)-Decimal(b))
