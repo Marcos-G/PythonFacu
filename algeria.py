7892fb2dd9b7a03b16838952b0425fd3043de3fb
Marcos Gómez <marnicgom@gmail.com>
2018-09-26 21:04:47 -0300

    Updated algeria.py.

--- algeria.py
+++ algeria.py
@@ -1,4 +1,16 @@
 from scipy.special import binom
+from math import ceil
+def probMujerGanarAntesDe(n):
+    if(n<1000):
+        pm=0
+    else:
+        pm=binom(n,1000)*(pow(0.5,n))
+def probHombreGanarTurno(n,t):
+    tg=ceil(1000/(2**(t-1)))
+    print(tg)
+    ph=binom(n-1,tg-1)*(pow(0.)
 h=11
 m=1
-print(binom(h,m))
+p=0
+n=1
+print(probHombreGanarTurno(1,h))
