181f7cc199c99bad3e43e44517cd86bf7886c16b
Marcos Gómez <marnicgom@gmail.com>
2018-10-05 11:42:23 -0300

    Updated intleg.py.

--- intleg.py
+++ intleg.py
@@ -1,7 +1,9 @@
 import sys
 
-if(len(sys.argv)>1):
-    file=sys.argv[1]
-    print(file)
-else:
+if(len(sys.argv)<2):
     print("FALTA RUTA DE PROGRAMA")
+    return
+if(not sys.argv[1].endswith(".ll")):
+    print("ARCHIVO INCORRECTO")
+    return
+print sys.argv[1]
